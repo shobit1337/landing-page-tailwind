b5c66882b79212f83bfdbc09b031b3437d15348e
shobit1337 <shobitdeshwal1337@gmail.com>
2022-04-24 12:10:17 +0530

    feat: basic landing page

---
+++ test.js
@@ -0,0 +1,18 @@
+console.log(1);
+const promise = new Promise((resolve) => {
+  console.log(2);
+  resolve();
+  console.log(3);
+});
+
+console.log(4);
+
+promise
+  .then(() => {
+    console.log(5);
+  })
+  .then(() => {
+    console.log(6);
+  });
+
+console.log(7);
